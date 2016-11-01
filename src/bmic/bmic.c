c5ceb741d4f2485e115a12e0da39d186d6451a4b
Otavio Rodolfo Piske <opiske@redhat.com>
2016-11-01 19:13:49 +0100

    NEW: modified the code to use the newer JSON features

--- src/bmic/bmic.c
+++ src/bmic/bmic.c
@@ -16,12 +16,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <transport/bmic_endpoint.h>
-#include <transport/bmic_endpoint_http.h>
-
 #include <common/gru_status.h>
 
-int main(int argc, char** argv) {
+#include <json-c/json.h>
+
+#include <transport/bmic_endpoint.h>
+#include <transport/bmic_endpoint_http.h>
+#include <format/bmic_json.h>
+
+
+int main(int argc, char** argv)
+{
     bmic_endpoint_t ep;
     gru_status_t status = {0};
 
@@ -30,17 +35,33 @@ int main(int argc, char** argv) {
     ep.url = (char *) argv[3];
     bmic_data_t reply = {0};
 
-    
+
     bmic_endpoint_http_begin(&ep, &status);
     if (status.code != GRU_SUCCESS) {
         printf("Unable to initialize HTTP endpoint: %s\n", status.message);
-        
+
         return EXIT_FAILURE;
     }
-    
+
     bmic_endpoint_http_read(&ep, NULL, &reply, &status);
-    printf("%s\n", reply.data);
+    printf("%s\n", (char *) reply.data);
     bmic_endpoint_http_terminate(&ep, &status);
+
+    bmic_json_t *json = bmic_json_init(reply.data, &status);
+    
+    if (status.code != GRU_SUCCESS) {
+        printf("Unable to initialize JSON data: %s\n", status.message);
+
+        return EXIT_FAILURE;
+    }
+
+    bmic_json_value_t value = {0};
+    
+    bmic_json_find_first(json, "QueueSize", &value);
+    printf("Queue size: %ld\n", value.data.number);
+    
+    bmic_json_find_first(json, "Name", &value);
+    printf("Name: %s\n", value.data.str);
 
     return (EXIT_SUCCESS);
 }
