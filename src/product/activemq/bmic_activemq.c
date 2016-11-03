a7d206db7ccfe9b81b0c641efa559df40fe1deda
Otavio R. Piske <angusyoung@gmail.com>
2016-11-03 22:24:33 +0100

    FIX: prevent incorrect addressing of status variable during json initialization

--- src/product/activemq/bmic_activemq.c
+++ src/product/activemq/bmic_activemq.c
@@ -1,12 +1,12 @@
 /**
  Copyright 2016 Otavio Rodolfo Piske
- 
+
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
- 
+
  http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,46 +22,46 @@ bmic_product_t *bmic_activemq_product(gru_status_t *status) {
     ret->product_init = bmic_activemq_init;
     ret->product_cleanup = bmic_activemq_cleanup;
     ret->product_info = bmic_activemq_product_info;
-    
+
     return ret;
 }
 
 const char *bmic_activemq_base_url(bmic_discovery_hint_t *hint) {
-    char *ret = NULL; 
-    
+    char *ret = NULL;
+
     if (hint->hint_type == URL) {
-        asprintf(&ret, ACTIVEMQ_BASE_URL_HINT_URL, 
+        asprintf(&ret, ACTIVEMQ_BASE_URL_HINT_URL,
                  hint->content.url);
     }
     else {
-        asprintf(&ret, ACTIVEMQ_BASE_URL_HINT_ADDRESSING, 
+        asprintf(&ret, ACTIVEMQ_BASE_URL_HINT_ADDRESSING,
                  hint->content.addressing.hostname, 8161);
     }
-    
+
     return ret;
 }
 
-bmic_handle_t *bmic_activemq_init(const char *base_url, 
-                                 bmic_credentials_t *credentials, 
+bmic_handle_t *bmic_activemq_init(const char *base_url,
+                                 bmic_credentials_t *credentials,
                                  gru_status_t *status) {
     bmic_handle_t *handle = NULL;
-    
+
     handle = gru_alloc(sizeof(bmic_handle_t), status);
     gru_alloc_check(handle, NULL);
-    
+
     handle->ep = bmic_endpoint_init(base_url, NULL, NULL, status);
     if (handle->ep == NULL) {
         bmic_activemq_cleanup(&handle);
-        
+
         return NULL;
     }
-    
+
     bmic_endpoint_set_credentials(handle->ep, credentials, status);
     bmic_endpoint_http_begin(handle->ep, status);
-    
+
     handle->transport.read = bmic_endpoint_http_read;
     handle->transport.write = bmic_endpoint_http_write;
-    
+
     return handle;
 }
 
@@ -69,32 +69,32 @@ void bmic_activemq_cleanup(bmic_handle_t **handle) {
     bmic_handle_destroy(handle, bmic_endpoint_http_terminate);
 }
 
-bmic_product_info_t *bmic_activemq_product_info(bmic_handle_t *handle, 
+bmic_product_info_t *bmic_activemq_product_info(bmic_handle_t *handle,
         gru_status_t *status)
 {
     bmic_data_t reply = {0};
-    
+
     bmic_endpoint_set_path(handle->ep, ACTIVEMQ_PRODUCT_INFO_PATH);
-    
+
     bmic_endpoint_status_t epstatus = {0};
     epstatus.status = status;
-    
+
     handle->transport.read(handle->ep, NULL, &reply, &epstatus);
     // printf("%s\n", (char *) reply.data);
-    
+
     if (status->code != GRU_SUCCESS) {
         printf("Error while trying to call the service: %d\n", epstatus.epcode);
         bmic_endpoint_reset_path(handle->ep);
         return NULL;
     }
-    
+
     bmic_endpoint_reset_path(handle->ep);
-    
-    bmic_json_t *json = bmic_json_init(reply.data, &status);
+
+    bmic_json_t *json = bmic_json_init(reply.data, status);
     if (json == NULL || status->code != GRU_SUCCESS) {
         return NULL;
     }
-    
+
     bmic_json_value_t value = {0};
     bmic_json_find_first(json, "value", &value);
 
@@ -102,10 +102,10 @@ bmic_product_info_t *bmic_activemq_product_info(bmic_handle_t *handle,
         printf("Version: %s\n", value.data.str);
 
         bmic_product_info_t *ret = gru_alloc(sizeof(bmic_product_t), status);
-        snprintf(ret->version, sizeof(ret->version), "%s", value.data.str); 
+        snprintf(ret->version, sizeof(ret->version), "%s", value.data.str);
 
         return ret;
-    }   
-    
+    }
+
     return NULL;
 }
--- src/product/artemis/bmic_artemis.c
+++ src/product/artemis/bmic_artemis.c
@@ -1,12 +1,12 @@
 /**
  Copyright 2016 Otavio Rodolfo Piske
- 
+
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
- 
+
  http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,46 +24,46 @@ bmic_product_t *bmic_artemis_product(gru_status_t *status) {
     ret->product_init = bmic_artemis_init;
     ret->product_info = bmic_artemis_product_info;
     ret->product_cleanup = bmic_artemis_cleanup;
-    
+
     return ret;
 }
 
 const char *bmic_artemis_base_url(bmic_discovery_hint_t *hint) {
-    char *ret = NULL; 
-    
+    char *ret = NULL;
+
     if (hint->hint_type == URL) {
-        asprintf(&ret, ARTEMIS_BASE_URL_HINT_URL, 
+        asprintf(&ret, ARTEMIS_BASE_URL_HINT_URL,
                  hint->content.url);
     }
     else {
-        asprintf(&ret, ARTEMIS_BASE_URL_HINT_ADDRESSING, 
+        asprintf(&ret, ARTEMIS_BASE_URL_HINT_ADDRESSING,
                  hint->content.addressing.hostname, 8161);
     }
-    
+
     return ret;
 }
 
-bmic_handle_t *bmic_artemis_init(const char *base_url, 
-                                 bmic_credentials_t *credentials, 
+bmic_handle_t *bmic_artemis_init(const char *base_url,
+                                 bmic_credentials_t *credentials,
                                  gru_status_t *status) {
     bmic_handle_t *handle = NULL;
-    
+
     handle = gru_alloc(sizeof(bmic_handle_t), status);
     gru_alloc_check(handle, NULL);
-    
+
     handle->ep = bmic_endpoint_init(base_url, NULL, NULL, status);
     if (handle->ep == NULL) {
-        bmic_artemis_cleanup(&handle);  
-        
+        bmic_artemis_cleanup(&handle);
+
         return NULL;
     }
-    
+
     bmic_endpoint_set_credentials(handle->ep, credentials, status);
     bmic_endpoint_http_begin(handle->ep, status);
-    
+
     handle->transport.read = bmic_endpoint_http_read;
     handle->transport.write = bmic_endpoint_http_write;
-    
+
     return handle;
 }
 
@@ -72,42 +72,42 @@ void bmic_artemis_cleanup(bmic_handle_t **handle) {
     bmic_handle_destroy(handle, bmic_endpoint_http_terminate);
 }
 
-bmic_product_info_t *bmic_artemis_product_info(bmic_handle_t *handle, 
+bmic_product_info_t *bmic_artemis_product_info(bmic_handle_t *handle,
         gru_status_t *status)
 {
     bmic_data_t reply = {0};
-    
+
     bmic_endpoint_set_path(handle->ep, ARTEMIS_PRODUCT_INFO_PATH);
-    
+
     bmic_endpoint_status_t epstatus = {0};
     epstatus.status = status;
-    
+
     handle->transport.read(handle->ep, NULL, &reply, &epstatus);
     // printf("%s\n", (char *) reply.data);
-    
+
     if (status->code != GRU_SUCCESS) {
         bmic_endpoint_reset_path(handle->ep);
         return NULL;
     }
-    
+
     bmic_endpoint_reset_path(handle->ep);
-    
-    bmic_json_t *json = bmic_json_init(reply.data, &status);
+
+    bmic_json_t *json = bmic_json_init(reply.data, status);
     if (json == NULL || status->code != GRU_SUCCESS) {
         return NULL;
     }
-    
+
     bmic_json_value_t value = {0};
     bmic_json_find_first(json, "value", &value);
-    
+
     if (value.type == STRING) {
         printf("Version: %s\n", value.data.str);
 
         bmic_product_info_t *ret = gru_alloc(sizeof(bmic_product_t), status);
-        snprintf(ret->version, sizeof(ret->version), "%s", value.data.str); 
+        snprintf(ret->version, sizeof(ret->version), "%s", value.data.str);
 
         return ret;
     }
-    
+
     return NULL;
 }
