88ce00d0d4096e1f4f8a73451cff7ce08c238213
Otavio R. Piske <angusyoung@gmail.com>
2016-11-03 21:28:36 +0100

    FIX: prevent a warning about non-void function not returning a value

--- src/management/discovery/bmic_discovery.c
+++ src/management/discovery/bmic_discovery.c
@@ -1,12 +1,12 @@
 /**
  Copyright 2016 Otavio Rodolfo Piske
- 
+
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
- 
+
  http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,7 +34,7 @@ static bool bmic_try_init(bmic_product_t *product,
         gru_dealloc_string(&base_url);
         return false;
     }
-    
+
     bmic_product_info_t *info = product->product_info(handle, &status);
 
     if (info) {
@@ -57,7 +57,7 @@ static bmic_product_info_t *bmic_discovery_registry_initializer(const gru_list_t
     gru_node_t *node = NULL;
 
     if (list == NULL) {
-        return;
+        return NULL;
     }
 
     node = list->root;
