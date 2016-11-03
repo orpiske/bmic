bc35bacaf6ff6d48caa8a9a7d61248b8aff5fd66
Otavio R. Piske <angusyoung@gmail.com>
2016-11-03 22:19:05 +0100

    FIX: prevents implicit declaration of the functions

--- src/product/bmic_product_register.h
+++ src/product/bmic_product_register.h
@@ -1,12 +1,12 @@
 /**
  Copyright 2016 Otavio Rodolfo Piske
- 
+
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
- 
+
  http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,6 +17,8 @@
 #define BMIC_PRODUCT_REGISTER_H
 
 #include "management/common/bmic_product.h"
+#include "product/activemq/bmic_activemq.h"
+#include "product/artemis/bmic_artemis.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -30,4 +32,3 @@ void bmic_product_unregister();
 #endif
 
 #endif /* BMIC_PRODUCT_REGISTER_H */
-
