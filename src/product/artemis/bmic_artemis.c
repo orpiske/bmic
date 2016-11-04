3f86744c8dcf73c4f2ee47b059aaade1bfc8120e
Otavio Rodolfo Piske <opiske@redhat.com>
2016-11-04 09:35:06 +0100

    FIX: prevent a memory leak by destroying the json object as soon as its not-needed

--- src/product/artemis/bmic_artemis.c
+++ src/product/artemis/bmic_artemis.c
@@ -99,7 +99,8 @@ bmic_product_info_t *bmic_artemis_product_info(bmic_handle_t *handle,
 
     bmic_json_value_t value = {0};
     bmic_json_find_first(json, "value", &value);
-
+    bmic_json_destroy(&json);
+    
     if (value.type == STRING) {
         printf("Version: %s\n", value.data.str);
 
