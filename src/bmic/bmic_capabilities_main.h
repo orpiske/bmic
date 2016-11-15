a4c8a20c7c9fe9e48f27fe8e7f58da0874ac403b
Otavio Rodolfo Piske <opiske@redhat.com>
2016-11-15 13:38:11 +0100

    FIX: fix missing license header

--- src/bmic/bmic_capabilities_main.h
+++ src/bmic/bmic_capabilities_main.h
@@ -1,14 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   bmic_capabilities_main.h
- * Author: opiske
- *
- * Created on November 6, 2016, 7:36 PM
+/**
+ Copyright 2016 Otavio Rodolfo Piske
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
  */
 
 #ifndef BMIC_CAPABILITIES_MAIN_H
