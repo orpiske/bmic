5fe3776ac9266180ffe13263389bcf7c5f1dd929
Otavio R. Piske <angusyoung@gmail.com>
2016-11-16 00:04:16 +0100

    FIX: fixes a dangerous buffer overflow

--- src/management/common/bmic_product.c
+++ src/management/common/bmic_product.c
@@ -1,12 +1,12 @@
 /**
  Copyright 2016 Otavio Rodolfo Piske
- 
+
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
  You may obtain a copy of the License at
- 
+
  http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing, software
  distributed under the License is distributed on an "AS IS" BASIS,
  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,41 +17,41 @@
 
 static gru_list_t *registry = NULL;
 
-bmic_api_interface_t *bmic_api_interface_new(const char *name, const char *version, 
+bmic_api_interface_t *bmic_api_interface_new(const char *name, const char *version,
                                   gru_status_t *status) {
     bmic_api_interface_t *ret = gru_alloc(sizeof(bmic_api_interface_t), status);
     gru_alloc_check(ret, NULL);
-    
-    
+
+
     snprintf(ret->name, sizeof(ret->name) - 1, "%s", name);
-    snprintf(ret->version, sizeof(ret->name) - 1, "%s", version);
-    
+    snprintf(ret->version, sizeof(ret->version) - 1, "%s", version);
+
     return ret;
 }
 
 
 void bmic_api_interface_destroy(bmic_api_interface_t **product) {
     bmic_api_interface_t *prod = *product;
-    
+
     if (!prod) {
         return;
     }
-    
+
     gru_dealloc((void **)product);
 }
 
 void bmic_product_registry_init(gru_status_t *status) {
     if (registry == NULL) {
         registry = gru_list_new(status);
-        
+
         if (registry == NULL) {
             return;
         }
     }
     else {
-        gru_status_set(status, GRU_FAILURE, 
+        gru_status_set(status, GRU_FAILURE,
                        "A product registry has already been initialized");
-        
+
         return;
     }
 }
