c5205dc88d22a1eb0106dab3daa23a62ba446d6a
Otavio Rodolfo Piske <opiske@redhat.com>
2016-11-18 14:53:25 +0100

    NEW: adds a new object to be used for storing argument information

---
+++ src/base/common/bmic_op_arg.c
@@ -0,0 +1,71 @@
+/**
+ Copyright 2016 Otavio Rodolfo Piske
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+#include "bmic_op_arg.h"
+
+
+
+bmic_op_arg_t *bmic_op_arg_new(gru_status_t *status) {
+    bmic_op_arg_t *ret = gru_alloc(sizeof(bmic_op_arg_t), status);
+    gru_alloc_check(ret, NULL);
+
+    return ret;
+}
+
+void bmic_op_arg_destroy(bmic_op_arg_t **ptr) {
+    bmic_op_arg_t *obj = *ptr;
+
+    if (obj == NULL) {
+        return;
+    }
+
+    if (obj->name != NULL) {
+        gru_dealloc_string(&obj->name);
+    }
+
+    if (obj->description != NULL) {
+        gru_dealloc_string(&obj->description);
+    }
+
+    if (obj->type!= NULL) {
+        gru_dealloc_string(&obj->type);
+    }
+
+    gru_dealloc((void **) ptr);
+}
+
+void bmic_op_arg_set_name(bmic_op_arg_t *arg, const char *name) {
+    assert(arg != NULL);
+
+    if (asprintf(&arg->name, "%s", name) == -1) {
+        fprintf(stderr, "Unable to allocate memory for saving the argacity name\n");
+    }
+}
+void bmic_op_arg_set_description(bmic_op_arg_t *arg, const char *description) {
+    assert(arg != NULL);
+
+    if (asprintf(&arg->description, "%s", description) == -1) {
+        fprintf(stderr, "Unable to allocate memory for saving the argacity description\n");
+    }
+}
+
+
+void bmic_op_arg_set_type(bmic_op_arg_t *arg, const char *type) {
+    assert(arg != NULL);
+
+    if (asprintf(&arg->type, "%s", type) == -1) {
+        fprintf(stderr, "Unable to allocate memory for saving the argacity typename\n");
+    }
+}
---
+++ src/base/common/bmic_op_arg.h
@@ -0,0 +1,72 @@
+/**
+ Copyright 2016 Otavio Rodolfo Piske
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+#ifndef BMIC_OP_ARG_H
+#define BMIC_OP_ARG_H
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+
+#include <common/gru_alloc.h>
+#include <common/gru_status.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct bmic_op_arg_t_ {
+    char *name;
+    char *type;
+    char *description;
+} bmic_op_arg_t;
+
+
+/**
+ * Creates a new argument object
+ * @param status status object to contain error information
+ * @return NULL if unable to allocate memory or a pointer to a new 
+ * bmic_op_arg_t structure
+ */
+bmic_op_arg_t *bmic_op_arg_new(gru_status_t *status);
+
+/**
+ * Sets the name
+ * @param arg the argument object to set the name
+ * @param name the name to set
+ */
+void bmic_op_arg_set_name(bmic_op_arg_t *arg, const char *name);
+
+/**
+ * Sets the description
+ * @param arg the argument object to set the name
+ * @param description the description to set
+ */
+void bmic_op_arg_set_description(bmic_op_arg_t *arg, const char *description);
+
+/**
+ * Sets the argument type
+ * @param arg the argument object to set the name
+ * @param type the name to set
+ */
+void bmic_op_arg_set_type(bmic_op_arg_t *arg, const char *type);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BMIC_OP_ARG_H */
+
