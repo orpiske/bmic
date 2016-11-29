3013e22473c458350ceca6b371b54ab755d7a32c
Otavio Rodolfo Piske <opiske@redhat.com>
2016-11-29 14:41:51 +0100

    NEW: added Artemis-specific json writer

---
+++ src/product/artemis/bmic_artemis_json.c
@@ -0,0 +1,50 @@
+/**
+ Copyright 2016 Otavio Rodolfo Piske
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+#include "bmic_artemis_json.h"
+
+/*
+ curl -u admin:admin -H "Content-Type: application/json" -X POST 
+ *  -d '{ 
+ *       "type":"EXEC", 
+ *        "mbean":"org.apache.activemq.artemis:brokerName=\"0.0.0.0\",module=Core,serviceType=Server,type=Broker", 
+ *        "operation": "createQueue(java.lang.String,java.lang.String)", 
+ *        "arguments": ["name", "address"] 
+ *      }'
+ * 
+ *      http://localhost:8161/jolokia/
+ */
+
+void bmic_artemis_json_create_queue(bmic_object_t *op, bmic_json_t *json, 
+                                    const char *name) 
+{
+    json_object *op_type = json_object_new_string("EXEC");
+    json_object_object_add(json->obj, "type", op_type);
+    
+    json_object *mbean = json_object_new_string(op->name);
+    json_object_object_add(json->obj, "mbean", mbean);
+    
+    json_object *create_queue = json_object_new_string(CREATE_CORE_QUEUE_SIG);
+    json_object_object_add(json->obj, "operation", create_queue);
+    
+    // Arguments
+    json_object *arguments = json_object_new_array();
+    json_object *arg_name = json_object_new_string(name);
+    json_object *address = json_object_new_string(name);
+    json_object_array_add(arguments, arg_name);
+    json_object_array_add(arguments, address);
+    json_object_object_add(json->obj, "arguments", arguments);
+    
+}
---
+++ src/product/artemis/bmic_artemis_json.h
@@ -0,0 +1,44 @@
+/**
+ Copyright 2016 Otavio Rodolfo Piske
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+#ifndef BMIC_ARTEMIS_JSON_H
+#define BMIC_ARTEMIS_JSON_H
+
+#include <common/gru_status.h>
+#include <common/gru_alloc.h>
+
+#include "base/format/bmic_json.h"
+
+/**
+ * @brief this is an interim solution for writing json data. I have to think
+ * more about it, but I have to get the code working. I will think more about 
+ * this in the future.
+ */
+
+#define CREATE_CORE_QUEUE_SIG "createQueue(java.lang.String,java.lang.String)"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* BMIC_ARTEMIS_JSON_H */
+
