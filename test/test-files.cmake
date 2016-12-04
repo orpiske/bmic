dc99315a1d71de958a3a9ce4dc8f913c114e4c5b
Otavio R. Piske <angusyoung@gmail.com>
2016-12-04 21:31:32 +0100

    UPD: organized the test code for Artemis and added sample files for versions 1.3.0 and 1.5.0, as well as simple test for those

--- test/test-files.cmake
+++ test/test-files.cmake
@@ -1,11 +1,27 @@
 # In all project subdirectories
-set(json-test-files simple/simple.json artemis/capabilities-v140.json)
+set(json-simple-test-files simple/simple.json)
 
 set(SHARED_DATA_OUTPUT_PATH ${CMAKE_BINARY_DIR}/../build/target/tests/share)
 
 # ... then traverse subdirectories
 foreach(file ${json-test-files}) 
-    message("Copying script file ${CMAKE_CURRENT_SOURCE_DIR}/base/format/${file} to runtime dir ${SHARED_DATA_OUTPUT_PATH}")
+    message("Copying test data ${CMAKE_CURRENT_SOURCE_DIR}/base/format/${file} to ${SHARED_DATA_OUTPUT_PATH}")
+    file(COPY ${CMAKE_CURRENT_SOURCE_DIR}/base/format/${file} DESTINATION ${SHARED_DATA_OUTPUT_PATH})
+endforeach(file)
+
+
+# In all project subdirectories
+set(json-artemis-files
+    artemis/artemis-v130.json
+    artemis/artemis-v140.json
+    artemis/artemis-v150.json
+)
+
+set(SHARED_DATA_OUTPUT_PATH ${CMAKE_BINARY_DIR}/../build/target/tests/share)
+
+# ... then traverse subdirectories
+foreach(file ${json-artemis-files}) 
+    message("Copying test data ${CMAKE_CURRENT_SOURCE_DIR}/base/format/${file} to ${SHARED_DATA_OUTPUT_PATH}")
     file(COPY ${CMAKE_CURRENT_SOURCE_DIR}/base/format/${file} DESTINATION ${SHARED_DATA_OUTPUT_PATH})
 endforeach(file)
 
