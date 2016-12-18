562615a7537bf0b5914e10b2f744ec6239774de7
Otavio R. Piske <angusyoung@gmail.com>
2016-12-18 12:38:16 +0100

    FIX: correctly install missing test file

--- test/test-files.cmake
+++ test/test-files.cmake
@@ -4,7 +4,7 @@ set(json-simple-test-files simple/simple.json)
 set(SHARED_DATA_OUTPUT_PATH ${CMAKE_BINARY_DIR}/../build/target/tests/share)
 
 # ... then traverse subdirectories
-foreach(file ${json-test-files}) 
+foreach(file ${json-simple-test-files}) 
     message("Copying test data ${CMAKE_CURRENT_SOURCE_DIR}/base/format/${file} to ${SHARED_DATA_OUTPUT_PATH}")
     file(COPY ${CMAKE_CURRENT_SOURCE_DIR}/base/format/${file} DESTINATION ${SHARED_DATA_OUTPUT_PATH})
 endforeach(file)
