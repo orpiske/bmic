8dea04183953de2cdc3818529eabae45323cba25
Otavio Rodolfo Piske <opiske@redhat.com>
2016-12-20 10:57:00 +0100

    UPD: additional formatting changes to the test build scripts

--- test/test-files.cmake
+++ test/test-files.cmake
@@ -4,37 +4,38 @@ set(json-simple-test-files simple/simple.json)
 set(SHARED_DATA_OUTPUT_PATH ${CMAKE_BINARY_DIR}/../build/target/tests/share)
 
 # ... then traverse subdirectories
-foreach(file ${json-simple-test-files}) 
-    message("Copying test data ${CMAKE_CURRENT_SOURCE_DIR}/base/format/${file} to ${SHARED_DATA_OUTPUT_PATH}")
-    file(COPY ${CMAKE_CURRENT_SOURCE_DIR}/base/format/${file} DESTINATION ${SHARED_DATA_OUTPUT_PATH})
+foreach(file ${json-simple-test-files})
+	message("Copying test data ${CMAKE_CURRENT_SOURCE_DIR}/base/format/${file} to ${SHARED_DATA_OUTPUT_PATH}")
+	file(COPY ${CMAKE_CURRENT_SOURCE_DIR}/base/format/${file} DESTINATION ${SHARED_DATA_OUTPUT_PATH})
 endforeach(file)
 
 
 # Artemis files
-set(json-artemis-files
-    artemis/artemis-v130.json
-    artemis/artemis-v140.json
-    artemis/artemis-v150.json
+set(
+	json-artemis-files
+	artemis/artemis-v130.json
+	artemis/artemis-v140.json
+	artemis/artemis-v150.json
 )
 
 set(SHARED_DATA_OUTPUT_PATH ${CMAKE_BINARY_DIR}/../build/target/tests/share)
 
 # ... then traverse subdirectories
-foreach(file ${json-artemis-files}) 
-    message("Copying test data ${CMAKE_CURRENT_SOURCE_DIR}/base/format/${file} to ${SHARED_DATA_OUTPUT_PATH}")
-    file(COPY ${CMAKE_CURRENT_SOURCE_DIR}/base/format/${file} DESTINATION ${SHARED_DATA_OUTPUT_PATH})
+foreach(file ${json-artemis-files})
+	message("Copying test data ${CMAKE_CURRENT_SOURCE_DIR}/base/format/${file} to ${SHARED_DATA_OUTPUT_PATH}")
+	file(COPY ${CMAKE_CURRENT_SOURCE_DIR}/base/format/${file} DESTINATION ${SHARED_DATA_OUTPUT_PATH})
 endforeach(file)
 
 
 # ActiveMQ files
 set(json-activemq-files
-    activemq/activemq-v5.14.1.json
+	activemq/activemq-v5.14.1.json
 )
 
 set(SHARED_DATA_OUTPUT_PATH ${CMAKE_BINARY_DIR}/../build/target/tests/share)
 
 # ... then traverse subdirectories
-foreach(file ${json-activemq-files}) 
-    message("Copying test data ${CMAKE_CURRENT_SOURCE_DIR}/base/format/${file} to ${SHARED_DATA_OUTPUT_PATH}")
-    file(COPY ${CMAKE_CURRENT_SOURCE_DIR}/base/format/${file} DESTINATION ${SHARED_DATA_OUTPUT_PATH})
+foreach(file ${json-activemq-files})
+	message("Copying test data ${CMAKE_CURRENT_SOURCE_DIR}/base/format/${file} to ${SHARED_DATA_OUTPUT_PATH}")
+	file(COPY ${CMAKE_CURRENT_SOURCE_DIR}/base/format/${file} DESTINATION ${SHARED_DATA_OUTPUT_PATH})
 endforeach(file)
