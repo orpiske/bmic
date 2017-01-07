6db06ed417241f2db74bfaa59b9880042b547ccc
Otavio R. Piske <angusyoung@gmail.com>
2017-01-07 22:10:40 +0100

    UPD: uses full stack protection by default and removes the redundant strong option

--- CMakeLists.default.cmake
+++ CMakeLists.default.cmake
@@ -7,15 +7,15 @@ if (CMAKE_COMPILER_IS_GNUCXX)
 			set(BMIC_DEBUG_DEFINES "-DBMIC_DEBUG=2")
 	endif (BUILD_WITH_EXTRA_DEBUG)
 
-	set(CMAKE_C_FLAGS "-Wall -Wshadow -Wconversion -std=c99 -fstrict-aliasing -fstack-protector-all -fstack-protector-strong ${CMAKE_USER_C_FLAGS}" CACHE STRING
+	set(CMAKE_C_FLAGS "-Wall -Wshadow -Wconversion -std=c99 -fstrict-aliasing -fstack-protector-all ${CMAKE_USER_C_FLAGS}" CACHE STRING
 		"Flags used by the compiler during all build types." FORCE
 	)
 
-	set(CMAKE_C_FLAGS_DEBUG "-DNDEBUG ${BMIC_DEBUG_DEFINES} -fdiagnostics-color=auto -g ${CMAKE_USER_C_FLAGS}" CACHE STRING
+	set(CMAKE_C_FLAGS_DEBUG "-DNDEBUG ${BMIC_DEBUG_DEFINES} -fstrict-aliasing -fstack-protector-all -fdiagnostics-color=auto -g ${CMAKE_USER_C_FLAGS}" CACHE STRING
 		"Flags used by the compiler during debug build." FORCE
 	)
 
-	set(CMAKE_C_FLAGS_RELEASE "-O2 -fomit-frame-pointer -D_FORTIFY_SOURCE=1 ${CMAKE_USER_C_FLAGS}" CACHE STRING
+	set(CMAKE_C_FLAGS_RELEASE "-O2 -fomit-frame-pointer -fstrict-aliasing -fstack-protector-all -D_FORTIFY_SOURCE=1 ${CMAKE_USER_C_FLAGS}" CACHE STRING
 		"Flags used by the compiler during release build." FORCE
 	)
 endif (CMAKE_COMPILER_IS_GNUCXX)
